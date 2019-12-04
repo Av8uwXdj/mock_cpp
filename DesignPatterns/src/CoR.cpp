cfb660b40ada52aaade322c1b59f04bf01cb7ef7
Av8uwXdj <develop.s7mvznyb@gmail.com>
2019-12-04 23:57:47 +0900

    add

---
+++ DesignPatterns/src/CoR.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <assert.h>
+
+namespace
+{
+	enum
+	{
+		CHAIN_HIT_CLASS = 2
+	};
+}
+
+//--------------------------------------------------
+// Chain of Responsibility の基底クラス
+class Base
+{
+public:
+
+	Base() : pNext(nullptr) {}
+	virtual ~Base() {}
+
+	// 次の処理クラスを設定
+	Base* SetNext(Base* pNext)
+	{
+		assert(pNext != nullptr);
+		this->pNext = pNext;
+		return this->pNext;
+	}
+
+	// 処理可能なら処理
+	int Request()
+	{
+		if (_CanExec() == true)
+		{
+			int value = _Exec();
+			return value;
+		}
+		else
+		{
+			if (pNext == nullptr)
+			{
+				std::cout << "全処理クラスで処理できなかった" << std::endl;
+			}
+			else
+			{
+				int value = this->pNext->Request();
+				return value;
+			}
+		}
+		return -1;
+	}
+
+private:
+
+	virtual int _Exec() = 0;
+	virtual bool _CanExec() = 0;
+
+private:
+
+	Base* pNext;
+};
+
+
+//--------------------------------------------------
+// Chain of Responsibility の派生クラス1
+class Chain1 : public Base
+{
+public:
+
+	Chain1() {}
+	virtual ~Chain1() {}
+
+private:
+
+	int _Exec()
+	{
+		return 1;
+	}
+
+	bool _CanExec()
+	{
+		if (CHAIN_HIT_CLASS == 1)
+		{
+			return true;
+		}
+		return false;
+	}
+};
+
+//--------------------------------------------------
+// Chain of Responsibility の派生クラス2
+class Chain2 : public Base
+{
+public:
+
+	Chain2() {}
+	virtual ~Chain2() {}
+
+private:
+
+	int _Exec()
+	{
+		return 2;
+	}
+
+	bool _CanExec()
+	{
+		if (CHAIN_HIT_CLASS == 2)
+		{
+			return true;
+		}
+		return false;
+	}
+};
+
+//--------------------------------------------------
+// Chain of Responsibility の派生クラス3
+class Chain3 : public Base
+{
+public:
+
+	Chain3() {}
+	virtual ~Chain3() {}
+
+private:
+
+	int _Exec()
+	{
+		return 3;
+	}
+
+	bool _CanExec()
+	{
+		if (CHAIN_HIT_CLASS == 3)
+		{
+			return true;
+		}
+		return false;
+	}
+};
---
+++ DesignPatterns/src/Main.cpp
@@ -0,0 +1,21 @@
+#include "CoR.cpp"
+
+int main()
+{
+	Chain1* chain1 = new Chain1();
+	Chain2* chain2 = new Chain2();
+	Chain3* chain3 = new Chain3();
+
+	// \’z
+	chain1->SetNext(chain2)->SetNext(chain3);
+
+	// ŽÀs
+	int value = chain1->Request();
+
+	// Œ‹‰Ê
+	std::cout << "result = " << value << std::endl;
+
+	delete chain2;
+	delete chain1;
+	return 0;
+}
